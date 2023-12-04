b659f90eb17f092b49e62bd8ff47aec39848ed75
ivanmarinoff <marinoffivan@gmail.com>
2023-12-04 15:53:57 +0200

    Add project admin settings

--- Library_online_catalog/library/admin.py
+++ Library_online_catalog/library/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import EventModel, ProfileModel
+
+
+class imageAdmin(admin.ModelAdmin):
+    list_display = ["event_name", "category", "description", "date", "event_image"]
+    list_filter = ["event_name", "date"]
+    search_fields = ['event_name']
+    ordering = ['-date']
+
+    def image_tag(self, obj):
+        if obj.event_image:
+            return format_html('<img src="{}" style="max-width:150px; max-height:150px"/>'.format(obj.event_image))
+        return None
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "profile_picture"]
+    list_filter = ["first_name", "last_name"]
+    search_fields = ['first_name']
+    ordering = ['-first_name']
+
+    def image_tag(self, obj):
+        if obj.profile_picture:
+            return format_html('<img src="{}" style="max-width:150px; max-height:150px"/>'.format(obj.profile_picture))
+        return None
+
+
+admin.site.register(EventModel, imageAdmin)
+admin.site.register(ProfileModel, UserModelAdmin)
