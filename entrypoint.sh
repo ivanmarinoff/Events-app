efafa65a6d61389db2f841eb87a444d049792dc0
Ivan Marinov <107050101+ivanmarinoff@users.noreply.github.com>
2023-12-05 15:20:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "Running Database Migrations"
 python manage.py makemigrations
 python manage.py migrate
 
-echo "Running app management commands"
+echo "Running app commands"
 
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver &
 exec "$@"
