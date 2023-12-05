b8646b11ebf66d62f86eac684ab2da12942b87e8
ivanmarinoff <marinoffivan@gmail.com>
2023-12-05 13:33:15 +0200

    Added entrypoint.sh file

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Install the dependencies"
+pip install -r requirements.txt
+
+
+echo "Running Database Migrations"
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Running app management commands"
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
+exec "$@"
